f459bfeb7479c6e9c7f58e4258ef2e90c0dae5e9
Chriford Siame <siamechrif@gmail.com>
2022-04-19 00:29:56 +0200

    prepare project for deployment

--- sku/settings.py
+++ sku/settings.py
@@ -81,15 +81,21 @@ WSGI_APPLICATION = 'sku.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'codepiece_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
