aa1b7139ae197e2a2368918b4f8fbd966aba7396
Chriford Siame <siamechrif@gmail.com>
2022-04-18 22:53:52 +0200

    setup pytest for django

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 from rest_framework import status
 # from unittest import TestCase
-class GetTest(TestCase):
+import pytest
+
+
+class TestApp(TestCase):
     
-    def test_home_test(self):
+    def test_home(self):
         response = self.client.get("")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
