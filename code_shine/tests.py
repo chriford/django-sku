17d73688deda91354d8ca6c86ccb9a5b366ed4e5
Chriford Siame <siamechrif@gmail.com>
2022-04-18 21:11:05 +0200

    setup for the homepage

--- code_shine/tests.py
+++ code_shine/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from rest_framework import status
+# from unittest import TestCase
+class GetTest(TestCase):
+    
+    def test_home_test(self):
+        response = self.client.get("")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        
 # Create your tests here.
--- code_shine/views.py
+++ code_shine/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views.generic import ListView
 
+class HomePage(ListView):
+    template_name = 'index.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        
+        context = super(HomePage, self).get_context_data(*args, **kwargs)
+        context['greet'] = "Hello world!"
+        return context
+    
+    def get_queryset(self):
+        data = [1,2,3,4]
+        return HttpResponse(f"{data}")
 # Create your views here.
