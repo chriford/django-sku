94c361fdae3939d2b95370c44840db10343222d7
Chriford Siame <siamechrif@gmail.com>
2022-04-26 20:15:49 +0200

    create new apps

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User
 
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "first_name",
+        "last_name",
+        "username",
+        "email",
+        "created"
+    ]
+    list_filter = [
+        "created",
+    ]
+    list_per_page = 12
 # Register your models here.
--- sku/settings.py
+++ sku/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
+    'teachers',
+    'pupils'
 ]
 
 MIDDLEWARE = [
@@ -137,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
